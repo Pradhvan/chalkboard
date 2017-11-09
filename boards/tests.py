247191687d984462294bff7d94d5671e9211940d
Pradhvan <Pradhvanbisht@gmail.com>
2017-11-09 16:36:39 +0530

    Added forum API and tests

--- boards/tests.py
+++ boards/tests.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import home, board_topics
+from .views import home, board_topics, new_topic
 from .models import Board
+from .forms import NewTopicForm
 
 
 class HomeTests(TestCase):
@@ -50,6 +51,16 @@ class BoardTopicsTests(TestCase):
         homepage_url = reverse('home')
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
 
+    def test_board_topics_view_contains_navigation_links(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+
+        response = self.client.get(board_topics_url)
+
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
 
 class NewTopicTests(TestCase):
     def setUp(self):
@@ -74,3 +85,20 @@ class NewTopicTests(TestCase):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+    def test_contains_form(self):  # <- new test
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
+    def test_new_topic_invalid_post_data(self):  # <- updated this one
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
